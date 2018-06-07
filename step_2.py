39eb825a2768288746bc678892146fe0a067e384
Yunfei <philipguo1992@gmail.com>
2018-06-07 18:16:03 -0300

    change code in step 2

--- step_2.py
+++ step_2.py
@@ -7,6 +7,7 @@ with open('tweets.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     posScore = 0
     negScore = 0
+    count = 0
     with open('tweets_score.csv', 'w') as new_file:
         csv_writer = csv.writer(new_file)
         csv_writer.writerow(['tweets', 'sentiment', 'score'])
@@ -16,20 +17,22 @@ with open('tweets.csv', 'r') as csv_file:
             tokens = nltk.word_tokenize(sentence)
             for token in tokens:
                 synsets = swn.senti_synsets(token)
+                count = count+1
                 for synst in synsets:
                     posScore = posScore + synst.pos_score()
                     negScore = negScore + synst.neg_score()
             if posScore > negScore:
                 sentiment = 'positive'
-                score = posScore
+                score = posScore/count
             elif negScore > posScore:
                 sentiment = 'negative'
-                score = negScore
+                score = -negScore/count
             elif negScore == posScore:
                 sentiment = 'neutral'
-                score = negScore
+                score = 0
             posScore = 0
             negScore = 0
+            count = 0
             csv_writer.writerow([row, sentiment, score])
 
 
